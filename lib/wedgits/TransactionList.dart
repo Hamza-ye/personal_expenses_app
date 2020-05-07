31241a63f9e2a8266ecdbe28295a9dedbecd4888
Hamza-ye <7amza.it@gmail.com>
2020-05-08 02:47:20 +0300

    make list scrollable

--- lib/wedgits/TransactionList.dart
+++ lib/wedgits/TransactionList.dart
@@ -10,44 +10,48 @@ class TransactionList extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2)),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              child: Row(
                 children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                  Container(
+                    child: Text(
+                      '\$${transactions[index].amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
                     ),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2)),
+                    padding: EdgeInsets.all(10),
                   ),
-                  Text(
-                    DateFormat().format(tx.date),
-                    style: TextStyle(color: Colors.grey),
-                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        DateFormat().format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  )
                 ],
-                crossAxisAlignment: CrossAxisAlignment.start,
-              )
-            ],
-          ),
-        );
-      }).toList(),
+              ),
+            );
+          },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
--- lib/wedgits/UserTransactions.dart
+++ lib/wedgits/UserTransactions.dart
@@ -13,6 +13,14 @@ class _UserTransactionsState extends State<UserTransactions> {
   final List<Transaction> _userTransactions = [
     Transaction(
         id: '001', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: '002', title: 'New Shoes1', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: '003', title: 'New Shoes2', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: '004', title: 'New Shoes3', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: '005', title: 'New Shoes4', amount: 69.99, date: DateTime.now()),
   ];
 
   void _addNewTransaction(String axTitle, double axAmount) {
