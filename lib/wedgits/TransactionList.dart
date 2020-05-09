e80e3b7d8dc20280598274a2a2e93119b0ed1226
Hamza-ye <7amza.it@gmail.com>
2020-05-10 00:38:58 +0300

    Improve the Overall Chart

--- lib/wedgits/TransactionList.dart
+++ lib/wedgits/TransactionList.dart
@@ -15,38 +15,20 @@ class TransactionList extends StatelessWidget{
       child: ListView.builder(
           itemBuilder: (context, index) {
             return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      '\$${transactions[index].amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
+              elevation: 5,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FittedBox(
+                        child: Text('\$${transactions[index].amount}')
                     ),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Theme.of(context).primaryColorLight, width: 2)),
-                    padding: EdgeInsets.all(10),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        transactions[index].title,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        DateFormat().format(transactions[index].date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                  )
-                ],
+                ),
+                title: Text('${transactions[index].title}'),
+                subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
               ),
             );
           },
