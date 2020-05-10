e225ac8f358a301dd0ef44cf5d4ebf8cb903723d
Hamza-ye <7amza.it@gmail.com>
2020-05-11 00:46:21 +0300

    Format code

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Personal Expenses',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
         textTheme: ThemeData.light().textTheme.copyWith(
           title: TextStyle(
@@ -44,16 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransactions = [
-//    Transaction(
-//        id: '001', title: 'New Shoes', amount: 10.00, date: DateTime.now()),
-//    Transaction(
-//        id: '002', title: 'Groceries', amount: 30.00, date: DateTime.now()),
-//    Transaction(
-//        id: '003', title: 'Taxi', amount: 9.11, date: DateTime.now()),
-//    Transaction(
-//        id: '004', title: 'New Shirt', amount: 10.00, date: DateTime.now()),
-  ];
+  final List<Transaction> _userTransactions = [];
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
